0790cdfe15eee9e714196e83490ceee6af15d996
sukantahui <huisukanta77@gmail.com>
2023-03-02 14:05:05 +0530

    helper file added

--- huigitpush.bat
+++ huigitpush.bat
@@ -1,9 +1,11 @@
+@echo off
 if [%1]==[] goto usage
 
 git add .
 git commit -m %1
 git pull
 git push
+goto :eof
 :usage
-@echo Usage: %0 ^<EnvironmentName^>
-exit /B 1
+@echo Please add a comment
+
